35048ec8b81f13c5b56a32b99df9187f72c866c0
Albert Augusto <167814598+AlbertAugusto11@users.noreply.github.com>
2025-07-25 19:08:52 -0300

    Deploy

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 dotenv.load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
